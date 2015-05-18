554ab3bc176c1e6241e1a813a4b115b6564fb428
eiji mishiro <eiji346g@gmail.com>
2015-05-18 18:37:26 +0700

    done 9.3

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,6 +2,26 @@
 describe "UserPages" do
     subject{page}
 
+
+    describe "edit" do
+        let(:user) { FactoryGirl.create(:user) }
+        before { visit edit_user_path(user) }
+
+        describe "page" do
+            it { should have_content("Update your profile") }
+            it { should have_title("Edit user") }
+            it { should have_link('change', href: 'http://gravatar.com/emails') }
+        end
+=begin
+        describe "with invalid information" do
+            before { click_button "Save changes" }
+
+            it { should have_content('error') }
+        end
+=end
+    end
+
+
     describe "signup page"do
         before{visit signup_path}
         it{should have_content('Sign up')}
