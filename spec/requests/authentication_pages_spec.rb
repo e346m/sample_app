dc6799be4d78b74fb06fae5a74e70b1a3b6402a3
eiji mishiro <eiji346g@gmail.com>
2015-05-24 19:35:39 +0700

    done 11.28

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -60,6 +60,16 @@ describe "Authentication" do
 
             describe "in the Users controller" do
 
+              describe "visiting the following page" do
+                before {visit following_user_path(user)}
+                it{should have_title('Sing in')}
+              end
+
+              describe "visiting the follower page" do
+                before{visit followers_user_path(user)}
+                it {should have_title('Sign in')}
+              end
+
                 describe "visiting the edit page" do
                     before { visit edit_user_path(user)}
                     it { should have_title('Sign in')}
