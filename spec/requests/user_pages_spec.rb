2b813ebf61ac0137e1d1b6409252a100b6657bf5
eiji mishiro <eiji346g@gmail.com>
2015-05-25 15:00:05 +0700

    done 11章課題

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -186,38 +186,48 @@ describe "UserPages" do
         end
       end
     end
-  end
-
-  describe "signup" do
-    before{visit signup_path}
-
-    let(:submit){"Create my account"}
-
-    describe "with invalid information"do
-      it "should not create a user" do
-        expect{click_button submit}.not_to change(User, :count)
-      end
-    end
-
-    describe "with valid information"do
+    describe "following/follower statistics" do
+      let(:other_user) { FactoryGirl.create(:user) }
       before do
-        fill_in"Name", with:"Example User"
-        fill_in "Email", with:"user@example.com"
-        fill_in "Password", with:"foobar"
-        fill_in "Confirm Password", with:"foobar"
-      end
-      it"should create a user"do
-        expect{click_button submit}.to change(User, :count).by(1)
+        other_user.follow!(user)
+        visit user_path(user)
       end
 
-      describe "after saving the user" do
-        before{click_button submit }
-        let(:user){User.find_by(email:'user@example.com')}
-        it{ should have_link('Sign out')}
-        it {should have_title(user.name)}
-        it{should have_selector('div.alert.alert-success', text:'Welcome')}
-      end
+      it { should have_link("0 following",  href: following_user_path(user)) }
+      it { should have_link("1 followers",  href: followers_user_path(user)) }
     end
   end
+
+describe "signup" do
+  before{visit signup_path}
+
+  let(:submit){"Create my account"}
+
+  describe "with invalid information"do
+    it "should not create a user" do
+      expect{click_button submit}.not_to change(User, :count)
+    end
+  end
+
+  describe "with valid information"do
+    before do
+      fill_in"Name", with:"Example User"
+      fill_in "Email", with:"user@example.com"
+      fill_in "Password", with:"foobar"
+      fill_in "Confirm Password", with:"foobar"
+    end
+    it"should create a user"do
+      expect{click_button submit}.to change(User, :count).by(1)
+    end
+
+    describe "after saving the user" do
+      before{click_button submit }
+      let(:user){User.find_by(email:'user@example.com')}
+      it{ should have_link('Sign out')}
+      it {should have_title(user.name)}
+      it{should have_selector('div.alert.alert-success', text:'Welcome')}
+    end
+  end
+end
 end
 
